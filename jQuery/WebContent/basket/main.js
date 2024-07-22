dae37aefaed3ac1b0e018e263acc9dc3afe99715
Jaewonnnn <yjw970710@gmail.com>
2024-07-22 09:08:13 +0900

    07.22

--- jQuery/WebContent/basket/main.js
+++ jQuery/WebContent/basket/main.js
@@ -1,31 +1,48 @@
 $(document).ready(function () {
-    $('.quantity').on('input change', function () {
-        if ($(this).val() <= 0) {
-            $(this).closest('.cart-item').find('input[type="checkbox"]').prop('checked', false);
-        }
-        else {
-            $(this).closest('.cart-item').find('input[type="checkbox"]').prop('checked', true);
-        }
-        calculateTotalPrice();
-    });
+  $(".quantity").on("input change", function () {
+    const quantityInput = $(this);
+    const cartItem = quantityInput.closest(".cart-item");
 
-    $('.remove').click(function () {
-        $(this).closest('.cart-item').remove();
-        calculateTotalPrice();
-    });
-
-    function calculateTotalPrice() {
-        let total = 0;
-        $('.cart-item').each(function () {
-            if ($(this).find('input[type="checkbox"]').is(':checked')) {
-                const price = parseInt($(this).find('.price').text().replace(/,/g, '').replace('원', '').trim());
-                const quantity = parseInt($(this).find('.quantity').val());
-                total += price * quantity;
-            }
-        });
-        $('#total-price').text(total.toLocaleString());
-        $('.checkout').text(`구매하기 (${$('.cart-item:has(input[type="checkbox"]:checked)').length})`);
+    if (quantityInput.val() <= 0) {
+      cartItem.find('input[type="checkbox"]').prop("checked", false);
+    } else {
+      cartItem.find('input[type="checkbox"]').prop("checked", true);
     }
 
     calculateTotalPrice();
+  });
+
+  $(".remove").click(function () {
+    $(this).closest(".cart-item").remove();
+    calculateTotalPrice();
+  });
+
+  // 체크박스 상태에 따라 총 가격 업데이트
+  $('input[type="checkbox"]').change(function () {
+    calculateTotalPrice();
+  });
+
+  function calculateTotalPrice() {
+    let total = 0;
+    $(".cart-item").each(function () {
+      if ($(this).find('input[type="checkbox"]').is(":checked")) {
+        const price = parseInt(
+          $(this)
+            .find(".price")
+            .text()
+            .replace(/,/g, "")
+            .replace("원", "")
+            .trim()
+        );
+        const quantity = parseInt($(this).find(".quantity").val());
+        total += price * quantity;
+      }
+    });
+    $("#total-price").text(total.toLocaleString());
+    $(".checkout").text(
+      `구매하기 (${$('.cart-item:has(input[type="checkbox"]:checked)').length})`
+    );
+  }
+
+  calculateTotalPrice();
 });
