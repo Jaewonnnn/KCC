4c46ec79a467a4a9d82c49d2f7567b1001ee51ed
Jaewonnnn <yjw970710@gmail.com>
2024-09-13 16:57:18 +0900

    09.13

--- Kcc_docker/spring_docker/src/main/java/com/kcc/spring_docker/controller/HelloController.java
+++ Kcc_docker/spring_docker/src/main/java/com/kcc/spring_docker/controller/HelloController.java
@@ -1,13 +1,26 @@
 package com.kcc.spring_docker.controller;
 
+import com.kcc.spring_docker.service.MemberService;
+import com.kcc.spring_docker.vo.MemberVO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
+
+    private final MemberService memberService;
 
     @GetMapping("/hello")
     public String hello(){
         return "Hello Docker2!!!";
+    }
+
+    @GetMapping("/members")
+    public List<MemberVO> selectAllMembers(){
+        return memberService.selectAllMembers();
     }
 }
